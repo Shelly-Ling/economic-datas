e07c27c1fcef6663e6102d8d01ac8c4b6579c5e1
Shelly-Ling <sontee.ling.chen@gmail.com>
2021-04-20 14:27:24 +0800

    test deploy

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+# npm run build
+# npm run start
+run generate
+
+# navigate into the build output directory
+cd dist
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f https://github.com/<USERNAME>/<USERNAME>.GitHub.io.git master
+
+# if you are deploying to https://<USERNAME>.Github.io/<REPO>
+# https://Shelly-Ling.Github.io/countdown-timer
+# git push -f https://github.com/<USERNAME>/<REPO>.git master:gh-pages
+# git push -f https://github.com/Shelly-Ling/countdown-timer.git master:gh-pages
+
+git push -f https://github.com/Shelly-Ling/economic-datas.git master:gh-pages
+
+cd -
